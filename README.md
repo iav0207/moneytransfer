43af7f8e8ad5dc5dbc38cdaf554232158a3d636e
Alexander Isaev <IsaevAV27@gmail.com>
2018-02-18 16:10:40 +0300

    Write simple demo flow script

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Run
 1. Run `mvn clean install` to build your application
 1. Start application with `java -jar target/money-transfer-1.0-SNAPSHOT.jar server config.yml`
 1. To check that your application is running enter url `http://localhost:8080` or `https://localhost:8443`
-
+1. To see an example API workflow run `python3 src/pytest/test_api_flow.py`
---
+++ src/pytest/test_api_flow.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import requests
+import urllib.parse
+from collections import defaultdict
+
+config = {
+    'base_url': 'http://localhost:8080'
+}
+
+log = logging.getLogger('test-api-logger')
+log.addHandler(logging.StreamHandler())
+log.setLevel('INFO')
+
+log.info(f'pwd: {os.getcwd()}')
+
+errors = []
+
+
+def uri(path):
+    return urllib.parse.urljoin(config['base_url'], path)
+
+
+def assert_that(msg, condition):
+    if not condition:
+        errors.append(msg)
+
+
+def post(url, json):
+    log.info(f'\tPOST : {url} : {json}')
+    resp = requests.post(url, json=json)
+    log.info(f'\tResponse: {resp.json()}')
+    assert_that('Response received', resp.json())
+    return resp
+
+
+def get(url):
+    log.info(f'\tGET : {url}')
+    resp = requests.get(url=url)
+    log.info(f'\tResponse: {resp.json()}')
+    assert_that('Response received', resp)
+    return resp
+
+
+def run():
+    log.info('\nDemo flow started.\n')
+
+    accounts = defaultdict(list)
+    usd = 840
+    rub = 643
+
+    log.info('Opening USD account...')
+
+    resp = post(uri('accounts/open'), json={'currency': usd})
+    accounts[usd].append(resp.json())
+
+    log.info('Getting just created account...')
+
+    resp = get(uri(f'accounts?id={accounts[usd][0]["id"]}'))
+    assert_that('Received account currency is USD', resp.json()['currencyCode'] == usd)
+
+    log.info('Creating another USD account...')
+
+    resp = post(uri('accounts/open'), json={'currency': usd})
+    assert_that('Account has different id', resp.json()['id'] != accounts[usd][0]['id'])
+    accounts[usd].append(resp.json())
+
+    log.info('Creating RUB account...')
+
+    resp = post(uri('accounts/open'), json={'currency': rub})
+    assert_that('Received account currency is RUB', resp.json()['currencyCode'] == rub)
+    accounts[rub].append(resp.json())
+
+    assert not errors, '\n'.join(errors)
+
+    log.info('\nSuccess.')
+
+
+if __name__ == '__main__':
+    run()
+
